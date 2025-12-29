589824a2a53529d21105012474fa02138d462aee
aminaelhasni-debug <aminaelhasni17@gmail.com>
2025-12-29 10:06:51 +0100

    calling the express and momgoose, connect mongoose, create router test

---
+++ index.js
@@ -0,0 +1,22 @@
+// calling the express and momgoose
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+// allows json to read and accept data 
+
+const app =  express();
+app.use(express.json());
+
+// connect mongoose
+
+mongoose.connect("mongodb://localhost:27017/eCommerce")
+.then (()=> console.log("done"))
+.catch(() => console.log("error"));
+
+app.listen(5000, () => { console.log ("server is runing in port 5000")});
+
+// router
+app.get("/test", (req, res) => {
+    res.send("hello test page")
+});
